43b885b67ff6281182923e4dd1f1fad76afa4faa
Solène Lefeu <solene.lefeu@epitech.eu>
2022-03-29 20:50:35 +0200

    [FIX] | clean text.hpp file + .cpp files

---
+++ IGraphic/Text.cpp
@@ -0,0 +1,36 @@
+#include "Text.hpp"
+
+namespace arcade {
+
+Text::Text(const vec2int& pos, const std::string_view &txt,
+    const color_uint8& color = {255, 255, 255},
+    const color_uint8 back_color = {0, 0, 0})
+        : pos_(pos), txt_(txt), color_(color), back_color_(back_color)
+{
+};
+
+void Text::setString(const std::string& txt) {
+    txt_ = txt;
+};
+std::string Text::getString() const {
+    return txt_;
+};
+void Text::setColor(const color_uint8& color) {
+    color_ = color;
+};
+color_uint8 Text::getColor() const {
+    return color_;
+};
+void Text::setBackColor(const color_uint8& back_color) {
+    back_color_ = back_color;
+};
+color_uint8 Text::getBackColor() const {
+    return back_color_;
+};
+void Text::setPos(const vec2int& pos) {
+    pos_ = pos;
+};
+vec2int Text::getPos() const {
+    return pos_;
+};
+}
--- justfile
+++ justfile
@@ -1,4 +1,4 @@
 format:
     find . -type f -name "*.cpp" -o -name "*.hpp" | xargs clang-format -i
 build:
-    cd build && rm -rf * && cmake .. && cmake --build . && cd ..
+    cd build && rm -rf * && cmake .. -GNinja && ninja && cd ..
